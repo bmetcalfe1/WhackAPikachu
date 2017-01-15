04a9392a849bf8c391ef7e66205e24408de17a10
Brendan Metcalfe <bgmetcalfe@gmail.com>
2017-01-15 13:42:35 -0500

    moles up and down timer

--- mole.js
+++ mole.js
@@ -2,6 +2,7 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
 
 function randomTime (min, max) {
 	return Math.round(Math.random() * (max - min) + min);
@@ -20,6 +21,11 @@ function randomHole (holes) {
 function peep() {
 	const time = randomTime(200, 1000);
 	const hole = randomHole(holes);
-	console.log(time, hole);
 	hole.classList.add('up');
+	setTimeout(() => {
+		hole.classList.remove('up');
+		if (!timeUp) {
+			peep();
+		}
+	}, time);
 }
